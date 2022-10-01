c645a68f9985e208566cc6064afe9b9acd2cf34a
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-01 22:30:58 +0300

    ejs, multer

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const multer = require("multer")
+const express=require("express")
+const app=express()
+
+const upload = multer({dest: "uploads"})
+
+app.set("view engine","ejs")
+app.get("/",(req,res) => {
+    res.render("index")
+})
+
+
+app.post("/upload", upload.single("file"), (req,res) => {
+    res.send("hello")
+})
+app.listen(3000)

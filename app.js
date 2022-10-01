9571cc53339ee3f36de201cc5b839195ca96f9e3
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-01 22:52:36 +0300

    dotenv, mongodb

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-
+require("dotenv").config()
 const multer = require("multer")
+const mongoose= require("mongoose")
+
+
 const express=require("express")
 const app=express()
 
 const upload = multer({dest: "uploads"})
+
+mongoose.connect(process.env.DATABASE_URI,(req,res)=>{
+    console.log("mongi ok")
+})
 
 app.set("view engine","ejs")
 app.get("/",(req,res) => {
@@ -14,4 +21,5 @@ app.get("/",(req,res) => {
 app.post("/upload", upload.single("file"), (req,res) => {
     res.send("hello")
 })
-app.listen(3000)
+app.listen(process.env.PORT,(req,res)=>{
+    console.log("server ok")})

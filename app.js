e1e33036602b04b4b17974bfc463119743edc578
İslam Uyanık <90751829+uyanikislam@users.noreply.github.com>
2022-10-02 22:20:32 +0300

    mongoose, bcrypt

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-require("dotenv").config()
-const multer = require("multer")
-const mongoose= require("mongoose")
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File= require("./models/File")
 
 
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-const upload = multer({dest: "uploads"})
+const upload = multer({ dest: "uploads" });
 
-mongoose.connect(process.env.DATABASE_URI,(req,res)=>{
-    console.log("mongi ok")
-})
+mongoose.connect(process.env.DATABASE_URI, (req, res) => {
+  console.log("mongi ok");
+});
 
-app.set("view engine","ejs")
-app.get("/",(req,res) => {
-    res.render("index")
-})
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+  const file = await File.create(fileData);
+  console.log(file);
+  res.send(file.originalName);
+});
 
-app.post("/upload", upload.single("file"), (req,res) => {
-    res.send("hello")
-})
-app.listen(process.env.PORT,(req,res)=>{
-    console.log("server ok")})
+app.listen(process.env.PORT, (req, res) => {
+  console.log("server ok");
+});
